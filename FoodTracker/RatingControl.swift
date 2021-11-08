2eda9e55ee03454bc5928f1671c954b57791b6e5
hammer-zren <hammer.zren@gmail.com>
2021-11-08 14:41:47 +1100

    food tracker app

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,19 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by ZHONGTAO REN on 18/1/19.
+//  Copyright © 2019 ZREN. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    //MARK: Initialization
+    
+    override init(frame: CGRect) {
+        <#code#>
+    }
+
+}
